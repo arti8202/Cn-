532be75a855f59cce7a53c1b2372b2e46959630c
arti kumari <ananyakaushik43502@gmail.com>
2026-07-31 22:13:23 +0530

    Add files via upload

---
+++ Client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *message = "Hello from client";
+    char buffer[1024] = {0};
+
+    // 1. Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // 2. Define server address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+ // 3. Connect to server
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    // 4. Send message
+    send(sock, message, strlen(message), 0);
+
+    // 5. Receive response
+    read(sock, buffer, 1024);
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+
+    return 0;
+}
+
+
